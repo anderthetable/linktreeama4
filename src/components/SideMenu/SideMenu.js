10da18e9381838a626961d964ea5a06d0059ef04
anderthetable <andertodlu@gmail.com>
2025-08-17 03:08:32 +0200

    klk

---
+++ src/components/SideMenu/SideMenu.js
@@ -0,0 +1,22 @@
+export default function SideMenu() {
+    const hamburguer_btn = document.getElementById('hamburguer-btn')
+    const close_btn = document.getElementById('close-btn')
+    const side_menu = document.getElementById('side-menu')
+    const overlay = document.getElementById('menu-overlay')
+
+    const openMenu = () => {
+        side_menu.classList.add('active')
+        overlay.classList.add('active')
+        document.body.style.overflow = 'hidden';
+    }
+    const closeMenu = () => {
+        side_menu.classList.remove('active')
+        overlay.classList.remove('active')
+        document.body.style.overflow = '';
+    }
+
+    hamburguer_btn.addEventListener('click', openMenu)
+    close_btn.addEventListener('click', closeMenu)
+
+
+}
