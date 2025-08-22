b6931d1dec62e4fc7192fa896aa9e3a6150b4956
anderthetable <andertodlu@gmail.com>
2025-08-22 17:27:56 +0200

    eeec

---
+++ src/components/Navigation/Navigation2.js
@@ -0,0 +1,14 @@
+import styles from './Navigation2.module.css'
+
+export default function Dropdown(){
+    const dropdowns = document.querySelectorAll(styles.dropdown)	
+
+    dropdowns.forEach(dropdown =>{
+        const menu = dropdown.querySelector(styles.dropdown_menu)
+        dropdown.addEventListener('mouseenter', () => {
+            menu.style.display = 'block'
+        })
+
+
+    })
+}
