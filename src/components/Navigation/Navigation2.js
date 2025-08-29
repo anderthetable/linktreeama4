556e4a2421864785073e6ffd45353798f66eddf8
anderthetable <andertodlu@gmail.com>
2025-08-29 16:34:20 +0200

    wow

--- src/components/Navigation/Navigation2.js
+++ src/components/Navigation/Navigation2.js
@@ -1,15 +1,19 @@
 import styles from './Navigation2.module.css'
 
 export default function Dropdown(){
-    const dropdowns = document.querySelectorAll(styles.dropdown)	
+    const dropdowns = document.querySelectorAll(`.${styles.dropdown}`)	
+    console.log(dropdowns)
 
     dropdowns.forEach(dropdown =>{
-        const menu = dropdown.querySelector(styles.dropdown_menu)
+        const menu = dropdown.querySelector(`.${styles.dropdown_menu}`)
         dropdown.addEventListener('mouseenter', () => {
             menu.style.display = 'block'
             menu.style.opacity = '1';
         })
-
+        dropdown.addEventListener('mouseleave', () => {
+            menu.style.display = 'none'
+            menu.style.opacity = '0';
+        })
 
     })
 }
